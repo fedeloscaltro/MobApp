ce70ca7297f0f6b51d922ba192d23759ce67dd6b
Fedeloscaltro <federico.scaltriti@libero.it>
2020-03-17 16:46:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import kivy
+from kivy.app import App  # makes all the graphics
+
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class MyGrid(Widget):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("Name: ", self.name.text, " Email: ", self.email.text)
+        self.name.text = ""
+        self.email.text = ""
+
+
+class MyApp(App):  # inheritance from App
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == '__main__':
+    MyApp().run()
