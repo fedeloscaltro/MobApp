422ea7d73814d3f054ca0cd4e8cdb8fdf0922cfd
Fedeloscaltro <federico.scaltriti@libero.it>
2020-03-23 10:52:22 +0100

    FloatLayout pt. 1

---
+++ main2.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+class MyApp2(App):
+    def build(self):
+        return FloatLayout()
+
+
+if __name__ == '__main2__':
+    MyApp2.run()
